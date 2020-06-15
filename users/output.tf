f1710f75c8c9e376b65dc5ddd60e870053efef57
Ben Lu <ayroblu@gmail.com>
2020-06-16 00:33:51 +0100

    Include output

---
+++ users/output.tf
@@ -0,0 +1,9 @@
+output access_key_encrypted_secret_benlu {
+  description = "encrypted secret benlu"
+  value       = module.user_ben.access_key_encrypted_secret
+}
+output access_key_id_benlu {
+  description = "access key id"
+  value       = module.user_ben.access_key_id
+}
+
